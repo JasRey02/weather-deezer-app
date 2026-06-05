8f8cf5d8d344ec2b7f87d3d1ae6f3c8b6e1fb32c
JasRey02 <jasrey@hotmail.es>
2026-06-04 22:13:59 -0600

    Preparado para render

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.get('/api/recommendation/:city', async (req, res) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto ${PORT}`);
